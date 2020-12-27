9ecf080456222b8cac2cdff2e91205fc879f6dd0
Leo Balduf <leobalduf@gmail.com>
2020-12-27 11:13:27 +0100

    config: add value scaling to mapping

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use crate::{HIGH, LOW};
 use serde::{Deserialize, Serialize};
 
 /// Configuration for one virtual device.
@@ -10,9 +11,26 @@ pub struct VirtualDeviceConfig {
     pub read_only: bool,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum ValueScaling {
+    Linear { from: u16, to: u16 },
+    Logarithmic,
+}
+
+impl Default for ValueScaling {
+    fn default() -> Self {
+        ValueScaling::Linear {
+            from: LOW,
+            to: HIGH,
+        }
+    }
+}
+
 /// Mapping of one virtual device to one port on a hardware device.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MappingConfig {
     pub device: String,
     pub port: u8,
+    #[serde(default)]
+    pub scaling: Option<ValueScaling>,
 }
